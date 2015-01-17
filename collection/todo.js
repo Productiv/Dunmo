05dff61a9d6172951408f223da2ac2489bda5d3a
Michael Goldman <goldmandrummer@gmail.com>
2015-01-17 00:34:30 -0500

    Add isDone to Todo && insertTodo

--- collection/todo.js
+++ collection/todo.js
@@ -3,6 +3,7 @@
  * Todo
  * ====
  * title : String
+ * isDone : Bool
  * dueAt : Date
  * importance : Integer<1,2,3>
  * totalLength : Number<seconds>
@@ -12,4 +13,16 @@
 
 Todos = new Mongo.Collection("todos");
 
+insertTodo = function (todo, callback) {
+	todo.title = todo.title || "New todo";
+	todo.isDone = todo.isDone || false;
 
+	if (!todo.dueAt) {
+		var today = new Date();
+		var todo.dueAt = new Date(today.getFullYear(), today.getMonth(), today.getDay(), 23, 59, 00, 00);
+	}
+
+	todo.importance = todo.importance || 3;
+	todo.totalLength = todo.totalLength || 1800;
+	todo.completedLength = todo.completedLength || 0;
+}
