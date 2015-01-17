2dae50deda385a569ef09b3000f6138ef230ad08
Jeremy Meyer <jjman505@gmail.com>
2015-01-17 04:11:09 -0500

    seeding started

---
+++ collection/seed.js
@@ -0,0 +1,27 @@
+var tasks = [
+  {
+    title: 'task 1',
+    dueAt: new Date(),
+    isDone: false,
+    importance: 2,
+    totalLength: 3600,
+    completedLength: 1800
+  },
+  {
+    title: 'task 2',
+    dueAt: new Date(),
+    isDone: false,
+    importance: 3,
+    totalLength: 1800
+  },
+  {
+    title: 'task 3',
+    dueAt: new Date(),
+    isDone: true,
+    importance: 1,
+    totalLength: 4800
+  }
+];
+
+
+
