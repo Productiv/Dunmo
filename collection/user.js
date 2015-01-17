4308c0b6758648e042e89473091778e7967f4a12
Jeremy Meyer <jjman505@gmail.com>
2015-01-17 14:17:08 -0500

    day tasks

--- collection/user.js
+++ collection/user.js
@@ -2,5 +2,50 @@
 Meteor.users.helpers({
   todos: function() {
     return Todos.find({ ownerId: this._id });
-  }
+  },
+
+  timeslots: function() {
+    var lastWeek = Timeslots.find({
+      ownerId: this._id,
+      date: {
+        $gte: Date.sevenDaysAgoStart(),
+        $lt: Date.todayStart()
+      }
+    });
+
+    var total = _.reduce(lastWeek, function(sum, item) {
+      return sum + item.totalLength;
+    });
+    var avgLength = total / lastWeek.length;
+
+    var slots = Timeslots.find({
+      ownerId: this._id,
+      date: { $gte: Date.todayStart() }
+    }).fetch();
+
+    var slotsObj = _.object(_.map(slots, function(slot) {
+      return [ slot.date, slot ];
+    }));
+
+    var minDate = _.min(slots, 'date');
+    var maxDate = _.max(slots, 'date');
+    var currentDate = minDate;
+    var ret = [ currentDate ];
+
+    while(currentDate < maxDate) {
+      currentDate.setDate(currentDate.getDate() + 1)
+      ret.push(currentDate);
+    }
+
+    ret = _.map(ret, function(date) {
+      return slotsObj[date] || {
+        ownerId: this._id,
+        date: avgDate,
+        totalLength: avgLength
+      };
+    });
+
+    return ret;
+  } // end of user.timeslots()
 });
+
