e5b0def94b894234b92486e856f5768a21059e73
Michael Goldman <goldmandrummer@gmail.com>
2015-01-20 22:12:14 -0500

    For teh merge issue

--- client/controllers/taskitem.js
+++ client/controllers/taskitem.js
@@ -19,24 +19,14 @@ Template.taskitem.helpers({
 });
 
 Template.taskitem.events({
-<<<<<<< Updated upstream
-  "click .complete": function(e) {
-    Meteor.user().updateTimeslot(this.remainingLength);
-    removeTodo(this._id);
-  },
-  "click .edit": function(e) {
-    $("#editModal").attr("data-todo-id", this._id);
-  }
-=======
-    "click button#complete-task": function (event) {
+    "click .complete": function(e) {
         Meteor.user().updateTimeslot(this.remainingLength);
         removeTodo(this._id);
     },
-    "click button#edit-task": function (event) {
-        $("div#edit-modal button.confirm-edit-task").attr("id", this._id);
+    "click .edit": function(e) {
+        $("#editModal").attr("data-todo-id", this._id);
     },
     "hover #due-date": function (event) {
         // body...
     }
->>>>>>> Stashed changes
 });
