f0b301372046da60c1b96d83ddad48647d57bb9b
Michael Goldman <goldmandrummer@gmail.com>
2015-02-02 01:16:02 -0500

    route, view, and controller for All Tasks view ready for sidebar nav

--- router.js
+++ router.js
@@ -8,6 +8,7 @@ Router.route('/', function () {
 
 Router.route('/tasks', function () {
   this.render('tasks');
+  Session.set('page', 'tasks');
   this.render('tasksNav', { to: 'navbar' });
   this.render('editModal', { to: 'modal' });
 });
@@ -15,6 +16,13 @@ Router.route('/tasks', function () {
 Router.route('/tasks/new', function () {
   this.render('newTask');
   this.render('newTaskNav', { to: 'navbar' });
+});
+
+Router.route('/tasks/all', function () {
+  Session.set('page', 'allTasks');
+  this.render('allTasks');
+  this.render('tasksNav', { to: 'navbar' });
+  this.render('editModal', { to: 'modal' });
 });
 
 Router.route('/pomodoro/:id', function () {
